fc7994a4b0540c15227a8459a818390aa9f92a8f
eqbit <eqbits@gmail.com>
2020-05-10 16:51:17 +0500

    allowed non-russian movies

--- src/services/addMovie.ts
+++ src/services/addMovie.ts
@@ -1,6 +1,6 @@
 import { AddMovieResolver } from '../modules/movie';
 import { getDetailedInfo } from '../api/tmdb';
-import { responseMessage, testFor } from '../utils';
+import { responseMessage } from '../utils';
 
 export const addMovieResolver = new AddMovieResolver();
 
@@ -8,25 +8,26 @@ export const addMovieService = async (tmdbId: number) => {
   try {
     const result = await getDetailedInfo(tmdbId);
     
-    if (testFor('onlyRussianLetters', result.title)) {
-      const response = await addMovieResolver.addMovie({
-        name: result.title,
-        genre: result.genres.map((genre) => genre.name).join(','),
-        directedBy: '',
-        writtenBy: '',
-        description: result.overview,
-        customDescription: '',
-        year: new Date(result.release_date).getFullYear(),
-        updated: `${new Date()}`,
-        trailer: '',
-        poster: `http://image.tmdb.org/t/p/w300${result.poster_path}`,
-        popularity: Math.floor(result.popularity),
-        tmdbId: `${result.id}`
-      });
-      
-      return response.name ? responseMessage(`${response.name} added to database successfully`) : response;
+    if (result.popularity < 10) {
+      return responseMessage(`Movie '${result.title}' is not popular enough`);
     }
-    return responseMessage(`No russian translation in movie '${result.title}'`);
+    
+    const response = await addMovieResolver.addMovie({
+      name: result.title,
+      genre: result.genres.map((genre) => genre.name).join(','),
+      directedBy: '',
+      writtenBy: '',
+      description: result.overview,
+      customDescription: '',
+      year: new Date(result.release_date).getFullYear(),
+      updated: `${new Date()}`,
+      trailer: '',
+      poster: `http://image.tmdb.org/t/p/w300${result.poster_path}`,
+      popularity: Math.floor(result.popularity),
+      tmdbId: `${result.id}`
+    });
+    
+    return response.name ? responseMessage(`${response.name} added to database successfully`) : response;
   } catch (e) {
     return responseMessage(e.message);
   }
